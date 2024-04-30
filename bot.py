972f6e17fcfa3d548ceb8ffb85038611ca79f955
Gatoxy <104008458+gatoxy@users.noreply.github.com>
2024-04-30 08:48:33 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,66 +1,58 @@
+import telebot
 import requests
-import threading
-import asyncio
-from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes, ConversationHandler, MessageHandler
+import os
+import tempfile
 
-# Estados
-LINK_INPUT = 1
+# Chave da API do Telegram
+CHAVE_API = "6914169408:AAF8iQV53vASbKiDHl7H2vRRLcDb85VhPqc"
 
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
-    user_name = update.effective_user.first_name
-    await update.message.reply_text(f"Olá {user_name}, seja bem-vindo ao bot stream!!")
-    await update.message.reply_text("Para baixar arquivos, aperte em /download.")
-    return ConversationHandler.END
+# Estabelece conexão com o bot do Telegram
+bot = telebot.TeleBot(CHAVE_API)
 
-async def download(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
-    await update.message.reply_text("Por favor, envie o link para o vídeo MP4 que deseja baixar.")
-    return LINK_INPUT
+# Manipuladores de mensagens
+@bot.message_handler(commands=['start'])
+def send_begin(message):
+    bot.reply_to(
+        message, 
+        f"Olá {message.from_user.first_name}, sou o Bot Roxa e estou aqui para te ajudar. Como posso te ajudar?\n\n"
+        "/baixar: Baixe vídeos de qualquer link"
+    )
 
-async def download_file(link, chat_id, context):
-    loop = asyncio.get_running_loop()
-    # Fazendo solicitação GET para o link do vídeo
-    response = requests.get(link, stream=True)
+@bot.message_handler(commands=['baixar'])
+def send_download(message):
+    bot.send_message(
+        message.chat.id, 
+        "Por favor, cole o link do vídeo que você deseja baixar."
+    )
 
-    # Verificando se a solicitação foi bem-sucedida
-    if response.status_code == 200:
-        # Enviando o vídeo como uma mensagem de documento (MP4) para o bot do Telegram
-        document_content = b''
-        for chunk in response.iter_content(chunk_size=8192):  # Ajuste o tamanho do chunk conforme necessário
-            document_content += chunk
-            # Enviando mensagem de progresso a cada 20 segundos
-            if len(document_content) % (20 * 1024 * 1024) == 0:
-                await loop.run_in_executor(None, context.bot.send_message, chat_id, f"Baixando... Tamanho atual: {len(document_content)//1024} KB")
-        await context.bot.send_message(chat_id, "Iniciando o download...")
-        await context.bot.send_document(chat_id, document=document_content, filename="video.mp4")
-        await context.bot.send_message(chat_id, "Download concluído!")
-    else:
-        await context.bot.send_message(chat_id, "Ocorreu um erro ao baixar o vídeo. Por favor, tente novamente.")
+@bot.message_handler(func=lambda message: True)
+def handle_download(message):
+    try:
+        bot.send_message(message.chat.id, 'Estou baixando o vídeo...')
 
-async def receive_link(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
-    user_name = update.effective_user.first_name
-    link = update.message.text
-    await update.message.reply_text("Iniciando o download...")
+        # Baixando vídeo pelo link
+        video_url = message.text
+        video_request = requests.get(video_url)
+        video_filename = video_url.split('/')[-1]
 
-    # Iniciando uma thread para baixar o arquivo
-    threading.Thread(target=lambda: asyncio.run(download_file(link, update.message.chat_id, context))).start()
+        # Salvar o arquivo temporário no diretório temporário do sistema
+        temp_directory = tempfile.gettempdir()
+        video_filepath = os.path.join(temp_directory, video_filename)
+        
+        with open(video_filepath, 'wb') as video_file:
+            video_file.write(video_request.content)
 
-    return ConversationHandler.END
+        bot.send_message(message.chat.id, 'Vídeo baixado com sucesso!')
 
-async def cancel(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
-    await update.message.reply_text("Operação cancelada.")
-    return ConversationHandler.END
+        # Enviando o vídeo
+        with open(video_filepath, 'rb') as video_file:
+            bot.send_video(message.chat.id, video_file)
 
-conversation_handler = ConversationHandler(
-    entry_points=[CommandHandler("download", download)],
-    states={
-        LINK_INPUT: [MessageHandler(None, receive_link)],
-    },
-    fallbacks=[CommandHandler("cancel", cancel)],
-)
+        # Removendo o arquivo temporário após o envio
+        os.remove(video_filepath)
 
-app = ApplicationBuilder().token("6914169408:AAF8iQV53vASbKiDHl7H2vRRLcDb85VhPqc").build()
-app.add_handler(CommandHandler("start", start))
-app.add_handler(conversation_handler)
+    except Exception as e:
+        bot.send_message(message.chat.id, f"Ocorreu um erro ao tentar baixar e enviar o vídeo: {str(e)}")
 
-app.run_polling()
+# Inicia a escuta de mensagens
+bot.infinity_polling()
