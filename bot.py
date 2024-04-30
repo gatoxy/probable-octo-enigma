b8e97dd8aba0036e79878f3832f1548887856843
Gatoxy <104008458+gatoxy@users.noreply.github.com>
2024-04-30 09:05:16 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,13 +1,27 @@
 import telebot
-import requests
 import os
-import tempfile
+import psycopg2
 
 # Chave da API do Telegram
-CHAVE_API = "6914169408:AAF8iQV53vASbKiDHl7H2vRRLcDb85VhPqc"
+CHAVE_API = "7111668089:AAHcJZrbuUnJjuSxwNlx0cED_iiYdtp03Mc"
+
+# Configurações do banco de dados PostgreSQL
+DB_HOST = 'dpg-coodqu2cn0vc738n8rt0-a'
+DB_NAME = 'dbtelebot'
+DB_USER = 'dbtelebot'
+DB_PASSWORD = 'WXNB1epZt6ATjbQq1pu6etxNrwuSjQRT'
 
 # Estabelece conexão com o bot do Telegram
 bot = telebot.TeleBot(CHAVE_API)
+
+# Função para conectar ao banco de dados PostgreSQL
+def connect_to_db():
+    return psycopg2.connect(
+        host=DB_HOST,
+        database=DB_NAME,
+        user=DB_USER,
+        password=DB_PASSWORD
+    )
 
 # Manipuladores de mensagens
 @bot.message_handler(commands=['start'])
@@ -32,27 +46,34 @@ def handle_download(message):
 
         # Baixando vídeo pelo link
         video_url = message.text
-        video_request = requests.get(video_url)
-        video_filename = video_url.split('/')[-1]
 
-        # Salvar o arquivo temporário no diretório temporário do sistema
-        temp_directory = tempfile.gettempdir()
-        video_filepath = os.path.join(temp_directory, video_filename)
-        
-        with open(video_filepath, 'wb') as video_file:
-            video_file.write(video_request.content)
+        # Simulando o download do vídeo
+        video_filename = video_url.split('/')[-1]
+        video_filepath = f'/path/to/your/local/directory/{video_filename}'
+
+        # Lendo o arquivo de vídeo como bytes
+        with open(video_filepath, 'rb') as video_file:
+            video_data = video_file.read()
+
+        # Conectando ao banco de dados
+        connection = connect_to_db()
+        cursor = connection.cursor()
+
+        # Inserindo o arquivo no banco de dados
+        cursor.execute("INSERT INTO videos (nome, dados) VALUES (%s, %s)", (video_filename, video_data))
+        connection.commit()
 
         bot.send_message(message.chat.id, 'Vídeo baixado com sucesso!')
 
-        # Enviando o vídeo
-        with open(video_filepath, 'rb') as video_file:
-            bot.send_video(message.chat.id, video_file)
+        # Enviando mensagem de confirmação
+        bot.send_message(message.chat.id, 'O vídeo foi baixado e armazenado com sucesso no banco de dados.')
 
-        # Removendo o arquivo temporário após o envio
-        os.remove(video_filepath)
+        # Fechando a conexão com o banco de dados
+        cursor.close()
+        connection.close()
 
     except Exception as e:
-        bot.send_message(message.chat.id, f"Ocorreu um erro ao tentar baixar e enviar o vídeo: {str(e)}")
+        bot.send_message(message.chat.id, f"Ocorreu um erro ao tentar baixar e armazenar o vídeo: {str(e)}")
 
 # Inicia a escuta de mensagens
 bot.infinity_polling()
